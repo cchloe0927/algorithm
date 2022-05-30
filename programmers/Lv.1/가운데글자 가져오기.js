7ee140e19607ac3ca401e409769b7d4ea20112db
이현정 <96729804+cchloe0927@users.noreply.github.com>
2022-05-30 02:34:34 +0000

    가운데 글자 가져오기

---
+++ programmers/Lv.1/가운데글자 가져오기.js
@@ -0,0 +1,23 @@
+//단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+function solution(s){
+  let answer = 0;
+  for(let i=0; i<s.length; i++){
+    if(s.length%2===0){
+      answer = s[s.length/2-1] + s[s.length/2]
+    } else {
+      answer = s[s.length/2-0.5]
+    }
+  }
+  return answer
+}
+
+// 아래는 테스트 코드입니다.
+let s = "qwer"
+console.log(solution(s))
+
+//월요일 업로드
+
+
+
+
