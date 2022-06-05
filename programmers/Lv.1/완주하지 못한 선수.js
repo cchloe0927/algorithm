a8b93087b1e97c3a20e4143611537bb369e988a1
이현정 <96729804+cchloe0927@users.noreply.github.com>
2022-06-05 03:42:34 +0000

    완주하지 못한 선수

---
+++ programmers/Lv.1/완주하지 못한 선수.js
@@ -0,0 +1,19 @@
+//수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+//마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+//참가자 중에는 동명이인이 있을 수 있습니다.
+
+function solution(participant, completion){
+  let a = participant.sort()
+  let b = completion.sort()
+
+  for (let i=0; i<participant.length; i++){
+    if(participant[i]!==completion[i]){
+      return participant[i]
+    }
+  }
+}
+
+// 아래는 테스트 코드입니다.
+let participant = ["leo", "kiki", "eden"]
+let completion = 	["eden", "kiki"]
+console.log(solution(participant, completion))
