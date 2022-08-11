ed757546ed89ad0127fcf15a3d16849aada1d451
hyunjeong <cchloe0927@gmail.com>
2022-08-11 17:20:38 +0900

    18108번

---
+++ baekjoon/입출력과 사칙연산/18108.js
@@ -0,0 +1,10 @@
+/*
+const fs = require('fs'); 
+const input = fs.readFileSync("/dev/stdin").toString().trim(); 
+*/
+
+//불기 연도를 서기 연도로 변환한 결과를 출력한다
+
+const input = 2541;
+
+console.log(input - 543);
