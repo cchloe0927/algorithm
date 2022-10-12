ce2a5deab5b1067897882e61882d55617e3bfbe2
이현정 <cchloe0927@gmail.com>
2022-10-12 09:47:20 +0900

    1546번

---
+++ baekjoon/1차원배열/1546.js
@@ -0,0 +1,29 @@
+//세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다. 일단 세준이는 자기 점수 중에 최댓값을 골랐다. 이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+//예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+//세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+
+const fs = require('fs');
+let input = fs
+  .readFileSync(__dirname + '/input.txt')
+  .toString()
+  .split('\n');
+
+let row1 = Number(input[0]);
+let row2Arr = input[1].split(" ");
+let row2ArrNum = row2Arr.map((num)=>Number(num));
+
+row2ArrNum.sort((a, b) => b - a );
+let maxScore = row2ArrNum[0];
+
+let value = 0;
+let sum = 0
+for(let i= 0; i<row1; i++){
+    value = row2Arr[i]/maxScore*100
+    sum += value
+}
+console.log(sum/row1);
+
+//console.log(row1);
+//console.log(row2Arr);
+//console.log(row2ArrNum);
+//console.log(maxScore);
