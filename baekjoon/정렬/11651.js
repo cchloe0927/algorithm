17d2346d7a26415200fb9e5457791d32d5acacd1
이현정 <cchloe0927@gmail.com>
2022-11-14 19:34:47 +0900

    11651번

---
+++ baekjoon/정렬/11651.js
@@ -0,0 +1,31 @@
+//2차원 평면 위의 점 N개가 주어진다. 좌표를 y좌표가 증가하는 순으로, y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+let fs = require('fs');
+let input = fs
+  .readFileSync(__dirname + '/input.txt') // "/dev/stdin"
+  .toString()
+  .trim()
+  .split('\n');
+//console.log(input);
+
+let N = input.shift();
+//console.log(N);
+
+let result = '';
+let coordinate = input.map((coords) =>
+  coords.split(' ').map((num) => Number(num))
+);
+//console.log(coordinate);
+
+coordinate.sort((a, b) => {
+  if (a[1] !== b[1]) {
+    return a[1] - b[1];
+  }
+  return a[0] - b[0];
+});
+//console.log(coordinate);
+
+coordinate.forEach((item) => {
+  result += `${item[0]} ${item[1]}\n`;
+});
+console.log(result);
