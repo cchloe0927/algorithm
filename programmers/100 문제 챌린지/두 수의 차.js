932081c43c3144f3da2a1ca0d185c91cd636fc60
이현정 <cchloe0927@gmail.com>
2022-11-25 08:13:40 +0900

    두 수의 차

---
+++ programmers/100 문제 챌린지/두 수의 차.js
@@ -0,0 +1,12 @@
+/*
+정수 num1과 num2가 주어질 때, num1과 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+*/
+
+function solution(num1, num2) {
+  var answer = num1 - num2;
+  return answer;
+}
+
+let num1 = 2;
+let num2 = 3;
+console.log(solution(num1, num2));
