869628079ce8e25ab8ca0b7c87903ffb323d6033
이현정 <cchloe0927@gmail.com>
2022-11-24 13:14:17 +0900

    폰켓몬

--- programmers/폰켓몬.js
+++ programmers/폰켓몬.js
@@ -7,19 +7,22 @@ N마리 폰켓몬의 종류 번호가 담긴 배열 nums가 매개변수로 주�
 
 function solution(nums) {
   let answer = 0;
-  let setArr = new Set(nums); //중복값이 있는 배열을 Set 객체로 만들어서 중족을 제거
+  let setArr = new Set(nums); //중복값이 있는 배열을 Set 객체로 만들어서 중복 제거
   let uniqueArr = [...setArr]; //Set 객체를 다시 배열로 반환
   console.log('uniqueArr: ', uniqueArr);
-  let choose = [];
-  for (let i = 0; i < uniqueArr.length - 1; i++) {
-    for (let j = i + 1; j < uniqueArr.length; i++) {
-      choose.push(uniqueArr[i]);
-    }
-    uniqueArr[i];
-    console.log(uniqueArr[i]);
-  }
+
+  let chooseCount = parseInt(nums.length / 2);
+
+  answer = uniqueArr.length > chooseCount ? chooseCount : uniqueArr.length;
   return answer;
 }
 
 let nums = [3, 1, 2, 3];
 console.log(solution(nums)); //2
+
+/*문제풀이
+최대만 많은 종류의 폰켓몬을 뽑기 위해서는 중복되는 숫자를 제거하고 나온 배열에서 다양한 경우의 수를 계산하면 되지만,
+주어진 nums에서 / 2한 수만큼만 선택할 수 있기때문에 굳이 다양한 경우의 수를 구할 필요없이
+중복을 제거한 배열의 길이가 선택할 수 있는 수보다 크면 선택할 수 있는 수를 출력하고 반대로, 
+작다면 중복을 제거한 배열의 길이를 출력해줌
+*/
