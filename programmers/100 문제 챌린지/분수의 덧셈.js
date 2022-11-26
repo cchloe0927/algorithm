761cbe7a63e542d26d8df26b91f42a054f0880f4
이현정 <cchloe0927@gmail.com>
2022-11-26 09:20:25 +0900

    분수의 덧셈

---
+++ programmers/100 문제 챌린지/분수의 덧셈.js
@@ -0,0 +1,26 @@
+/*
+첫 번째 분수의 분자와 분모를 뜻하는 denum1, num1, 두 번째 분수의 분자와 분모를 뜻하는 denum2, num2가 매개변수로 주어집니다.
+두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+*/
+
+function solution(denum1, num1, denum2, num2) {
+  // 분자
+  let topNum = num1 * denum2 + num2 * denum1;
+  // 분모
+  let botNum = num1 * num2;
+  // 최소 공배수
+  let maximum = 1;
+  // 약분
+  for (let i = 1; i <= topNum; i++) {
+    if (topNum % i === 0 && botNum % i === 0) {
+      maximum = i;
+    }
+  }
+  return [topNum / maximum, botNum / maximum];
+}
+
+let denum1 = 1;
+let num1 = 2;
+let denum2 = 3;
+let num2 = 4;
+console.log(solution(denum1, num1, denum2, num2));
