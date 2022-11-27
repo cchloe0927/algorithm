8c7845283dacc4ff3a402d8fb20fd339e1579a44
이현정 <cchloe0927@gmail.com>
2022-11-27 12:17:17 +0900

    나머지 구하기

--- programmers/100 문제 챌린지/나머지 구하기.js
+++ programmers/100 문제 챌린지/나머지 구하기.js
@@ -1,12 +1,13 @@
 /*
-정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+정수 num중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다.
+정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
 */
 
-function solution(num1, num2) {
-  var answer = num1 % num2;
-  return answer;
+function solution(array) {
+  let arr = array.sort((a, b) => a - b);
+  let answer = Math.floor(arr.length / 2);
+  return arr[answer];
 }
 
-let num1 = 3;
-let num2 = 2;
-console.log(solution(num1, num2));
+let array = [1, 2, 7, 10, 11];
+console.log(solution(array));
