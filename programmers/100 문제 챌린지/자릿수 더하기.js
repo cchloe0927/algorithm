bbe8ef076140e6ad4340f2dfe2b2e72d4c57e7db
hyunjeong <cchloe0927@gmail.com>
2022-12-11 02:44:16 +0900

    자릿수 더하기

---
+++ programmers/100 문제 챌린지/자릿수 더하기.js
@@ -0,0 +1,14 @@
+/**
+정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요
+ */
+
+function solution(n) {
+  var answer = String(n)
+    .split("")
+    .map((num) => Number(num))
+    .reduce((a, b) => a + b, 0);
+  return answer;
+}
+
+let n = 1234;
+console.log(solution(n));
